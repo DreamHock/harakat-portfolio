2fe549dd78a98881f659e1e24adac7e8b929b470
harakaty6 <harakaty6@gmail.com>
2024-11-04 13:17:06 +0100

    project initialization

---
+++ assets/app.ts
@@ -0,0 +1,7 @@
+import "./bootstrap.js";
+import { registerReactControllerComponents } from "@symfony/ux-react";
+
+registerReactControllerComponents(
+  require.context("./react/controllers", true, /\.(j|t)sx?$/)
+);
+import "./styles/app.css";
--- assets/bootstrap.js
+++ assets/bootstrap.js
@@ -1,4 +1,4 @@
-import { startStimulusApp } from '@symfony/stimulus-bundle';
+const { startStimulusApp } = require("@symfony/stimulus-bridge");
 
 const app = startStimulusApp();
 // register any custom, 3rd party controllers here
